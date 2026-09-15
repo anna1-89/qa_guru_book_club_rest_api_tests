17c9df3b6995bbfa39a7bda98a8ed2d01206b88c
Anna Pilyugina <anna1_89@mail.ru>
2026-09-15 19:38:05 +0300

    Added exanples with pojo

---
+++ src/test/java/models/pojo/RegistrationResponsePojoModel.java
@@ -0,0 +1,70 @@
+package models.pojo;
+
+import static java.lang.String.format;
+
+public class RegistrationResponsePojoModel {
+    Integer id;
+    String username;
+    String firstName;
+    String lastName;
+    String email;
+    String remoteAddr;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemoteAddr() {
+        return remoteAddr;
+    }
+
+    public void setRemoteAddr(String remoteAddr) {
+        this.remoteAddr = remoteAddr;
+    }
+
+
+
+//    public RegistrationBodyPojoModel(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//    }
+
+
+
+}
---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,172 @@
+package tests;
+
+import models.pojo.RegistrationBodyPojoModel;
+import models.pojo.RegistrationResponsePojoModel;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegistrationTests {
+
+    @Test
+    public void successfulRegistrationTest() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().firstName();
+        String password = faker.name().firstName();
+
+        String data = "";
+
+        given()
+                .log().all()
+                .contentType(JSON)
+//                .header("content-type", ContentType.JSON)
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
+
+    }
+
+    @Test
+    public void successfulRegistrationTest_with_pojo() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().firstName();
+        String password = faker.name().firstName();
+
+        RegistrationBodyPojoModel data = new RegistrationBodyPojoModel();
+        data.setUsername(username);
+        data.setPassword(password);
+
+
+//        With constructor
+//        RegistrationBodyPojoModel data = new RegistrationBodyPojoModel(username, password);
+
+        RegistrationResponsePojoModel registrationResponse = given()
+                .log().all()
+                .contentType(JSON)
+//                .header("content-type", ContentType.JSON)
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .as(RegistrationResponsePojoModel.class);
+
+                assertEquals(username, registrationResponse.getUsername());
+
+
+    }
+
+    @Test
+    public void existingUser400Test() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().firstName();
+        String password = faker.name().firstName();
+
+        String data = "";
+
+        given()
+                .log().all()
+                .contentType(JSON)
+//                .header("content-type", ContentType.JSON)
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
+
+        given()
+                .log().all()
+                .contentType(JSON)
+//                .header("content-type", ContentType.JSON)
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("username[0]", is("A user with that username is already exists."));
+
+    }
+
+    @Test
+    public void invalidUsername400Test() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().fullName();
+        String password = faker.name().firstName();
+
+        String data = "";
+
+        given()
+                .log().all()
+                .contentType(JSON)
+//                .header("content-type", ContentType.JSON)
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
+
+    }
+
+    @Test
+    public void negativeRegistration500Test() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().fullName();
+        String password = faker.name().firstName();
+
+        String data = "";
+
+        given()
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
+
+    }
+
+    @Test
+    public void nunsupportedMediaType415Test() {
+        //https://book-club.qa.guru/api/v1/users/register/
+        Faker faker = new Faker();
+        String username = faker.name().fullName();
+        String password = faker.name().firstName();
+
+        String data = "";
+
+        given()
+                .body(data)
+                .when()
+                .post("https://book-club.qa.guru/api/v1/users/register/")
+                .then()
+                .statusCode(201)
+                .body("username", is(username))
+                .body("id", notNullValue());
+
+    }
+}
