2d9294f3771b7f5201db3592d6d3e267c7249d14
Anna Pilyugina <anna1_89@mail.ru>
2026-09-25 18:32:27 +0300

    added steps for logout tests

--- src/test/java/tests/LogoutTests.java
+++ src/test/java/tests/LogoutTests.java
@@ -5,6 +5,7 @@ import models.logout.EmptyTokenLogoutResponseModel;
 import models.logout.InvalidTokenLogoutResponseModel;
 import models.logout.LogoutBodyModel;
 import models.logout.SuccessfulLogoutResponseModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -18,6 +19,7 @@ import static tests.TestData.*;
 public class LogoutTests extends TestBase {
 
     @Test
+    @DisplayName("Успешный выход из системы")
     public void successfulLogoutTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_USERNAME, LOGIN_PASSWORD);
 
@@ -44,74 +46,92 @@ public class LogoutTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Ошибка при попытке выхода из системы без токена")
     public void emptyTokenLogoutTest() {
         LogoutBodyModel logoutData = new LogoutBodyModel("");
-        EmptyTokenLogoutResponseModel logoutResponse = given(baseRequestSpec)
-                .body(logoutData)
-                .when()
-                .post("/auth/logout/")
-                .then()
-                .spec(emptyTokenLogoutResponseSpec)
-                .extract().as(EmptyTokenLogoutResponseModel.class);
 
-        String expectedError = LOGOUT_EMPTY_TOKEN_ERROR;
-        String actualError = logoutResponse.refresh().get(0);
-        assertThat(actualError).isEqualTo(expectedError);
+        step("Отправка запроса logout без токена и проверка ответа (400, текст с ошибкой)", () -> {
+            EmptyTokenLogoutResponseModel logoutResponse = given(baseRequestSpec)
+                    .body(logoutData)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(emptyTokenLogoutResponseSpec)
+                    .extract().as(EmptyTokenLogoutResponseModel.class);
+
+            String expectedError = LOGOUT_EMPTY_TOKEN_ERROR;
+            String actualError = logoutResponse.refresh().get(0);
+            assertThat(actualError).isEqualTo(expectedError);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке выхода из системы с невалидным токеном")
     public void invalidTokenLogoutTest() {
         LogoutBodyModel logoutData = new LogoutBodyModel(LOGOUT_WRONG_TOKEN);
-        InvalidTokenLogoutResponseModel logoutResponse = given(baseRequestSpec)
-                .body(logoutData)
-                .when()
-                .post("/auth/logout/")
-                .then()
-                .spec(invalidTokenLogoutResponseSpec)
-                .extract().as(InvalidTokenLogoutResponseModel.class);
 
-        String expectedError = LOGOUT_WRONG_TOKEN_ERROR;
-        String expectedCodeError = LOGOUT_WRONG_TOKEN_CODE_ERROR;
-        String actualError = logoutResponse.detail();
-        String actualCodeError = logoutResponse.code();
-        assertThat(actualError).isEqualTo(expectedError);
-        assertThat(actualCodeError).isEqualTo(expectedCodeError);
+        step("Отправка запроса logout с невалидным токеном и проверка ответа (401, текст с ошибкой)", () -> {
+            InvalidTokenLogoutResponseModel logoutResponse = given(baseRequestSpec)
+                    .body(logoutData)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(invalidTokenLogoutResponseSpec)
+                    .extract().as(InvalidTokenLogoutResponseModel.class);
+
+            String expectedError = LOGOUT_WRONG_TOKEN_ERROR;
+            String expectedCodeError = LOGOUT_WRONG_TOKEN_CODE_ERROR;
+            String actualError = logoutResponse.detail();
+            String actualCodeError = logoutResponse.code();
+            assertThat(actualError).isEqualTo(expectedError);
+            assertThat(actualCodeError).isEqualTo(expectedCodeError);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке выхода из системы с уже использованным токеном")
     public void expiredTokenLogoutTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_USERNAME, LOGIN_PASSWORD);
-        String refreshToken = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(successfulLoginResponseSpec)
-                .extract().path("refresh");
 
-        LogoutBodyModel logoutData = new LogoutBodyModel(refreshToken);
-        SuccessfulLogoutResponseModel firstLogoutResponse = given(baseRequestSpec)
-                .body(logoutData)
-                .when()
-                .post("/auth/logout/")
-                .then()
-                .spec(successfulLogoutResponseSpec)
-                .extract().as(SuccessfulLogoutResponseModel.class);
+        String refreshToken = step("Авторизация и получение токена", () ->
+                given(baseRequestSpec)
+                        .body(loginData)
+                        .when()
+                        .post("/auth/token/")
+                        .then()
+                        .spec(successfulLoginResponseSpec)
+                        .extract().path("refresh"));
 
-        InvalidTokenLogoutResponseModel secondLogoutResponse = given(baseRequestSpec)
-                .body(logoutData)
-                .when()
-                .post("/auth/logout/")
-                .then()
-                .spec(invalidTokenLogoutResponseSpec)
-                .extract().as(InvalidTokenLogoutResponseModel.class);
+        step("Отправка запроса logout с refresh-токеном", () -> {
+            LogoutBodyModel logoutData = new LogoutBodyModel(refreshToken);
 
-        String expectedError = LOGOUT_BLACKLISTED_TOKEN_ERROR;
-        String expectedCodeError = LOGOUT_WRONG_TOKEN_CODE_ERROR;
-        String actualError = secondLogoutResponse.detail();
-        String actualCodeError = secondLogoutResponse.code();
-        assertThat(actualError).isEqualTo(expectedError);
-        assertThat(actualCodeError).isEqualTo(expectedCodeError);
+            SuccessfulLogoutResponseModel logoutResponse = given(baseRequestSpec)
+                    .body(logoutData)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(successfulLogoutResponseSpec)
+                    .extract().as(SuccessfulLogoutResponseModel.class);
+        });
+
+        step("Отправка повторного запроса logout с refresh-токеном и проверка ответа (401, текст с ошибкой)", () -> {
+            LogoutBodyModel logoutData = new LogoutBodyModel(refreshToken);
+
+            InvalidTokenLogoutResponseModel secondLogoutResponse = given(baseRequestSpec)
+                    .body(logoutData)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(invalidTokenLogoutResponseSpec)
+                    .extract().as(InvalidTokenLogoutResponseModel.class);
+
+            String expectedError = LOGOUT_BLACKLISTED_TOKEN_ERROR;
+            String expectedCodeError = LOGOUT_WRONG_TOKEN_CODE_ERROR;
+            String actualError = secondLogoutResponse.detail();
+            String actualCodeError = secondLogoutResponse.code();
+            assertThat(actualError).isEqualTo(expectedError);
+            assertThat(actualCodeError).isEqualTo(expectedCodeError);
+        });
     }
 
 
