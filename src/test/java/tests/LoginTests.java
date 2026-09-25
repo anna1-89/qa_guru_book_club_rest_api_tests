81ca003a790714cecbd336fd93a611cba3013ef5
Anna Pilyugina <anna1_89@mail.ru>
2026-09-25 18:48:21 +0300

    added steps for login tests

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,8 +6,10 @@ import models.login.WrongCredentialsLoginResponseModel;
 import models.login.EmptyPasswordResponseModel;
 import models.login.EmptyUserAndPasswordResponseModel;
 import models.login.EmptyUsernameResponseModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static specs.BaseSpec.baseRequestSpec;
@@ -17,81 +19,95 @@ import static tests.TestData.*;
 public class LoginTests extends TestBase {
 
     @Test
+    @DisplayName("Успешный вход в систему")
     public void successfulLoginTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_USERNAME, LOGIN_PASSWORD);
 
-        SuccessfulLoginResponseModel loginResponse = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(successfulLoginResponseSpec)
-                .extract().as(SuccessfulLoginResponseModel.class);
+        step("Отправка запроса login и проверка ответа (200)", () -> {
+            SuccessfulLoginResponseModel loginResponse = given(baseRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(successfulLoginResponseSpec)
+                    .extract().as(SuccessfulLoginResponseModel.class);
 
-        String expectedTokenPath = LOGIN_TOKEN_PREFIX;
-        String actualAccess = loginResponse.access();
-        String actualRefresh = loginResponse.refresh();
-        assertThat(actualAccess).startsWith(expectedTokenPath);
-        assertThat(actualRefresh).startsWith(expectedTokenPath);
-        assertThat(actualAccess).isNotEqualTo(actualRefresh);
+            String expectedTokenPath = LOGIN_TOKEN_PREFIX;
+            String actualAccess = loginResponse.access();
+            String actualRefresh = loginResponse.refresh();
+            assertThat(actualAccess).startsWith(expectedTokenPath);
+            assertThat(actualRefresh).startsWith(expectedTokenPath);
+            assertThat(actualAccess).isNotEqualTo(actualRefresh);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке входа в систему с невалидным паролем")
     public void wrongCredentialsPasswordLoginTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_USERNAME, LOGIN_WRONG_PASSWORD);
 
-        WrongCredentialsLoginResponseModel loginResponse = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(wrongCredentialsLoginResponseSpec)
-                .extract().as(WrongCredentialsLoginResponseModel.class);
+        step("Отправка запроса login с невалидеым паролем и проверка ответа (401, текст с ошибкой)", () -> {
+            WrongCredentialsLoginResponseModel loginResponse = given(baseRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(wrongCredentialsLoginResponseSpec)
+                    .extract().as(WrongCredentialsLoginResponseModel.class);
 
-        String expectedDetailError = LOGIN_WRONG_CREDENTIALS_ERROR;
-        String actualDetailError = loginResponse.detail();
-        assertThat(actualDetailError).isEqualTo(expectedDetailError);
+            String expectedDetailError = LOGIN_WRONG_CREDENTIALS_ERROR;
+            String actualDetailError = loginResponse.detail();
+            assertThat(actualDetailError).isEqualTo(expectedDetailError);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке входа в систему с невалидным username")
     public void wrongCredentialUsernamesLoginTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_WRONG_USERNAME, LOGIN_PASSWORD);
 
-        WrongCredentialsLoginResponseModel loginResponse = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(wrongCredentialsLoginResponseSpec)
-                .extract().as(WrongCredentialsLoginResponseModel.class);
+        step("Отправка запроса login с невалидеым username и проверка ответа (401, текст с ошибкой)", () -> {
+            WrongCredentialsLoginResponseModel loginResponse = given(baseRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(wrongCredentialsLoginResponseSpec)
+                    .extract().as(WrongCredentialsLoginResponseModel.class);
 
-        String expectedDetailError = LOGIN_WRONG_CREDENTIALS_ERROR;
-        String actualDetailError = loginResponse.detail();
-        assertThat(actualDetailError).isEqualTo(expectedDetailError);
+            String expectedDetailError = LOGIN_WRONG_CREDENTIALS_ERROR;
+            String actualDetailError = loginResponse.detail();
+            assertThat(actualDetailError).isEqualTo(expectedDetailError);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке входа в систему с незаполненным username")
     public void emptyUserRegistrationTest() {
         LoginBodyModel loginData = new LoginBodyModel("", LOGIN_PASSWORD);
 
-        EmptyUsernameResponseModel loginResponse = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(emptyUserLoginResponseSpec)
-                .extract().as(EmptyUsernameResponseModel.class);
+        step("Отправка запроса login с незаполненным username и проверка ответа (400, текст с ошибкой)", () -> {
+            EmptyUsernameResponseModel loginResponse = given(baseRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(emptyUserLoginResponseSpec)
+                    .extract().as(EmptyUsernameResponseModel.class);
 
-        String expectedError = LOGIN_EMPTY_FIELD_ERROR;
-        String actualError = loginResponse.username().get(0);
-        assertThat(actualError).isEqualTo(expectedError);
+            String expectedError = LOGIN_EMPTY_FIELD_ERROR;
+            String actualError = loginResponse.username().get(0);
+            assertThat(actualError).isEqualTo(expectedError);
+        });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке входа в систему с незаполненным паролем")
     public void emptyPasswordRegistrationTest() {
         LoginBodyModel loginData = new LoginBodyModel(LOGIN_WRONG_USERNAME, "");
 
-        EmptyPasswordResponseModel login = given(baseRequestSpec)
+        step("Отправка запроса login с незаполненным паролем и проверка ответа (400, текст с ошибкой)", () -> {
+        EmptyPasswordResponseModel loginResponse = given(baseRequestSpec)
                 .body(loginData)
                 .when()
                 .post("/auth/token/")
@@ -100,28 +116,32 @@ public class LoginTests extends TestBase {
                 .extract().as(EmptyPasswordResponseModel.class);
 
         String expectedError = LOGIN_EMPTY_FIELD_ERROR;
-        String actualError = login.password().get(0);
+        String actualError = loginResponse.password().get(0);
         assertThat(actualError).isEqualTo(expectedError);
+    });
     }
 
     @Test
+    @DisplayName("Ошибка при попытке входа в систему с незаполненными username и паролем")
     public void emptyUserAndPasswordRegistrationTest() {
         LoginBodyModel loginData = new LoginBodyModel("", "");
 
-        EmptyUserAndPasswordResponseModel loginResponse = given(baseRequestSpec)
-                .body(loginData)
-                .when()
-                .post("/auth/token/")
-                .then()
-                .spec(emptyUserAndPasswordLoginResponseSpec)
-                .extract().as(EmptyUserAndPasswordResponseModel.class);
+        step("Отправка запроса login с незаполненными username и паролем и проверка ответа (400, текст с ошибкой)", () -> {
+            EmptyUserAndPasswordResponseModel loginResponse = given(baseRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(emptyUserAndPasswordLoginResponseSpec)
+                    .extract().as(EmptyUserAndPasswordResponseModel.class);
 
-        String expectedUsernameError = LOGIN_EMPTY_FIELD_ERROR;
-        String expectedPasswordError = LOGIN_EMPTY_FIELD_ERROR;
-        String actualUsernameError = loginResponse.username().get(0);
-        String actualPasswordError = loginResponse.password().get(0);
-        assertThat(actualUsernameError).isEqualTo(expectedUsernameError);
-        assertThat(actualPasswordError).isEqualTo(expectedPasswordError);
+            String expectedUsernameError = LOGIN_EMPTY_FIELD_ERROR;
+            String expectedPasswordError = LOGIN_EMPTY_FIELD_ERROR;
+            String actualUsernameError = loginResponse.username().get(0);
+            String actualPasswordError = loginResponse.password().get(0);
+            assertThat(actualUsernameError).isEqualTo(expectedUsernameError);
+            assertThat(actualPasswordError).isEqualTo(expectedPasswordError);
+        });
     }
 
 
